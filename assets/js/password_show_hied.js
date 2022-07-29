4f62560a818a1fdd78c55aaa86192bdf454ee1cf
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-07-29 14:41:45 +0200

    Chat users frontend part using javascript

--- assets/js/password_show_hied.js
+++ assets/js/password_show_hied.js
@@ -3,8 +3,15 @@ const pswrd = document.querySelector(".form .field input[type='password']"),
 
 toggleBtn.onclick = () => {
     if (pswrd.type == "password") {
+
         pswrd.type = 'text';
+
+        toggleBtn.classList.add("active");
+
     } else {
+
         pswrd.type = 'password';
+
+        toggleBtn.classList.remove('active');
     }
 }
