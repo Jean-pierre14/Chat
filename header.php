ec504ceee9cd57dc076a9662d5817b43667756ec
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-07-30 10:10:51 +0200

    Header

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Racine14 | chat</title>
+    <link rel="stylesheet" href="./assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="./assets/css/style.chat.css">
+</head>
--- login.php
+++ login.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Racine14 | chat</title>
-    <link rel="stylesheet" href="./assets/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link rel="stylesheet" href="./assets/css/style.chat.css">
-</head>
+<?php include_once "./header.php";?>
 
 <body>
     <div class="wrapper">
@@ -35,6 +24,7 @@
             <div class="link">I don't have an account <a href="index.php">Sign Up</a></div>
         </section>
     </div>
+    
     <script src="./assets/js/password_show_hied.js"></script>
 </body>
 
